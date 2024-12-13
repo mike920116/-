457e556bd4d72f09230cb0d6be4df33ac53b4f01
mike920116 <10856037@ntub.edu.tw>
2024-12-13 10:15:14 +0800

    index.html主畫面的內容

--- 宏仁專題課程/1213/1客戶更改/app.py
+++ 宏仁專題課程/1213/1客戶更改/app.py
@@ -15,7 +15,36 @@ app = Flask(__name__)
 #主畫面
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
+
+#客戶清單
+@app.route('/customer/list')
+def customer_list(): 
+    #取得資料庫連線 
+    connection = db.get_connection() 
+    
+    #產生執行sql命令的物件   
+    cursor = connection.cursor() 
+    
+    #執行SQL    
+    cursor.execute('SELECT cusno, cusname, address, contactor FROM customer order by cusno;')
+    
+    #取回SQL執行後的所有資料
+    data = cursor.fetchall()
+    
+    #設定參數, 準備傳給網頁
+    if data:
+        #如果有資料
+        params = [{'cusno': d[0], 'cusname': d[1], 'address': d[2], 'contactor': d[3]} for d in data]
+    else:
+        #如果無資料
+        params = None
+          
+    #關閉資料庫連線    
+    connection.close() 
+    
+    #將參數送給網頁, 讓資料嵌入網頁中  
+    return render_template('/customer/list.html', data=params) 
 
 #客戶新增表單
 @app.route('/customer/create/form')
