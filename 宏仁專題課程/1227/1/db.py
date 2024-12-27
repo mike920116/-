13e310ef9da90a9b7ef724b341e90c107b84bcc8
mike920116 <10856037@ntub.edu.tw>
2024-12-27 09:40:43 +0800

    將db密碼改成正確的

--- 宏仁專題課程/1227/1/db.py
+++ 宏仁專題課程/1227/1/db.py
@@ -5,7 +5,7 @@ DB_HOST = "localhost"
 DB_PORT = 3306  # MySQL 預設埠號
 DB_NAME = "north" #想要連到的資料庫名稱
 DB_USER = "root" #啟動超級使用者模式修改
-DB_PASSWORD = "11056$$$" #預設密碼11056$$$ #筆電localhost密碼0000
+DB_PASSWORD = "0000" #預設密碼11056$$$ #筆電localhost密碼0000
 
 # 建立資料庫連線
 def get_connection():
